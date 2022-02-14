c39ecfbec3111ff035928c2e41a5c30771f49126
MrOcumare <ilyaocumare@gmail.com>
2022-02-14 16:49:24 +0300

    added first lab and added SequenceСreator file

---
+++ LABS/LABS/ DoNotTouch/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  LABS
+//
+//  Created by Ilya Murashko on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
