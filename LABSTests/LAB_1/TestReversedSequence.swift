099af433bf71ed12fef70af4be3ca546fbd27fa6
MrOcumare <ilyaocumare@gmail.com>
2022-02-20 00:47:05 +0300

    checking valid testing flow

--- LABSTests/LAB_1/TestReversedSequence.swift
+++ LABSTests/LAB_1/TestReversedSequence.swift
@@ -14,23 +14,23 @@ class TestReversedSequence: XCTestCase {
         XCTAssertEqual( [1,2,3,4,5], SequenceСreator.createSequenceFromZeroTo(finalElement: 5))
     }
     
-    func testCreateSequenceFromZeroToWhenGetNegativeValue() {
-        XCTAssertEqual( [], SequenceСreator.createSequenceFromZeroTo(finalElement: -1))
-    }
-    
-    func testCreateSequenceFromZeroToWhenGetZeroValue() {
-        XCTAssertEqual( [], SequenceСreator.createSequenceFromZeroTo(finalElement: 0))
-    }
+//    func testCreateSequenceFromZeroToWhenGetNegativeValue() {
+//        XCTAssertEqual( [], SequenceСreator.createSequenceFromZeroTo(finalElement: -1))
+//    }
+//
+//    func testCreateSequenceFromZeroToWhenGetZeroValue() {
+//        XCTAssertEqual( [], SequenceСreator.createSequenceFromZeroTo(finalElement: 0))
+//    }
     
     func testCreateReversedSequenceToZeroFrom() {
         XCTAssertEqual( [5,4,3,2,1], SequenceСreator.createReversedSequenceToZeroFrom(finalElement: 5))
     }
     
-    func testCreateReversedSequenceToZeroFromWhenGetNegativeValue() {
-        XCTAssertEqual( [], SequenceСreator.createReversedSequenceToZeroFrom(finalElement: -1))
-    }
-    
-    func testCreateReversedSequenceToZeroFromWhenGetZeroValue() {
-        XCTAssertEqual( [], SequenceСreator.createReversedSequenceToZeroFrom(finalElement: 0))
-    }
+//    func testCreateReversedSequenceToZeroFromWhenGetNegativeValue() {
+//        XCTAssertEqual( [], SequenceСreator.createReversedSequenceToZeroFrom(finalElement: -1))
+//    }
+//
+//    func testCreateReversedSequenceToZeroFromWhenGetZeroValue() {
+//        XCTAssertEqual( [], SequenceСreator.createReversedSequenceToZeroFrom(finalElement: 0))
+//    }
 }
