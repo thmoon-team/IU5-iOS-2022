f230a153ffb2a1a783bcca3ed2642b4d71c27982
MrOcumare <ilyaocumare@gmail.com>
2022-02-19 13:41:11 +0300

    added manifest

---
+++ Tests/IU5-iOS-2022Tests/IU5_iOS_2022Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import IU5_iOS_2022
+
+final class IU5_iOS_2022Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(IU5_iOS_2022().text, "Hello, World!")
+    }
+}
