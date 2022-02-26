3f3046d75027978c7181cf8fdd4b41ae61297425
MrOcumare <ilyaocumare@gmail.com>
2022-02-26 14:01:48 +0300

    added reference counting task

---
+++ LABS/LAB_2/ReferenceCycleResolver.swift
@@ -0,0 +1,41 @@
+//
+//  ReferenceCycleResolver.swift
+//  LABS
+//
+//  Created by Ilya Murashko on 25.02.2022.
+//
+
+import Foundation
+
+// В реализации классов Student и University есть ошибка, приводящая к утечке памяти, найдите ее и исправьте.
+
+// Решение обоснуйте в блоке | Обоснование решения |.
+// Например: /* Я сделал …, т.к. это красиво */
+
+/* | Обоснование решения | */
+
+class Student {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    var university: University?
+}
+
+
+
+class University {
+    let universityName: String
+    init(universityName: String) {
+        self.universityName = universityName
+    }
+    var student: Student?
+}
+
+final class ReferenceCycleResolver {
+    static func universityAdmission(student: Student, to university: University) {
+        student.university = university
+        university.student = student
+    }
+}
+
