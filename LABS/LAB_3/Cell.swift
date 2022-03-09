cf44d0474d803b5368ead54d0d3103e56a35217b
Pritex007 <pritex009@gmail.com>
2022-03-09 02:24:31 -0800

    start lab3

---
+++ LABS/LAB_3/Cell.swift
@@ -0,0 +1,23 @@
+//
+//  Cell.swift
+//  LABS
+//
+//  Created by Погиблов on 08.03.2022.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LABS/LAB_3/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  LABS
+//
+//  Created by Погиблов on 08.03.2022.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
