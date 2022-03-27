13d3744b35d059b99acd6659cc403f7b0cb0dd85
Pritex007 <pritex009@gmail.com>
2022-03-27 20:10:41 +0300

    lab3

--- LABS/LAB_3/AppDelegate.swift
+++ LABS/LAB_3/AppDelegate.swift
@@ -1,25 +1,28 @@
 //
 //  AppDelegate.swift
-//  LABS
+//  lab3
 //
-//  Created by Ilya Murashko on 14.02.2022.
+//  Created by  on 09.03.2022.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+    
+        let initialViewController = InitialViewController()
+        let navigationViewController = UINavigationController(rootViewController: initialViewController)
+        navigationViewController.view.backgroundColor = .white
+        
+        window?.rootViewController = navigationViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
 }
-
